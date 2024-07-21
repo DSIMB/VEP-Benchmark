d262df927c0f6203dfbe26b1dfff81f586bc08d9
ragouradja <ragou.radja@gmail.com>
2024-07-21 19:24:34 +0200

    clean script

--- scripts/all_pipeline_light.sh
+++ scripts/all_pipeline_light.sh
@@ -4,14 +4,6 @@
 # This script automates the generation of input files and runs various tools for variant effect prediction.
 # Usage: ./vep_predictions.sh /path/to. /path/to/variant_file mode genome_reference
 
-# Variables #
-script_folder="./scripts/"
-input_folder="./input_files/"
-predictions_folder="./predictions/"
-dbNSFP_data_folder="/dsimb/glaciere/radjasan/dbNSFP/academic/"
-phdsnp_folder="${script_folder}/PhDSNP/PhD-SNPg/"
-suspect_folder="${script_folder}/SuSPect/suspect_package/"
-
 # Functions #
 usage() {
     echo "Usage: $0 -f /path/to/variant_file -m [panno|ganno] -g [38|37]"
@@ -71,9 +63,25 @@ fi
 
 # Activate conda environment
 
+# Variables #
+output_folder="./$output_name"
+script_folder="./scripts/"
+database_folder="./variant_databases/"
+input_folder="$output_folder/input_files/"
+predictions_folder="$output_folder/predictions/"
+phdsnp_folder="${script_folder}/PhDSNP/PhD-SNPg/"
+suspect_folder="${script_folder}/SuSPect/suspect_package/"
+dbnsfp_folder="$predictions_folder/dbNSFP"
+dbnsfp_output="$dbnsfp_folder/dbNSFP_output.tsv"
+dbNSFP_data_folder="$database_folder/dbNSFP/"
+
+
 # Create necessary directories
+echo "Creating $input_folder"
+echo "mkdir -p $input_folder"
 mkdir -p $input_folder
 mkdir -p $predictions_folder
+
 
 # # Main Script #
 
@@ -82,7 +90,7 @@ SECONDS=0
 if [ "$mode" == "panno" ]; then
     echo "Mode: Protein annotation (panno)"
     echo "Generating input files: genevar, idvar, geneseq, ESM, VariPred"
-    python $script_folder/generate_input.py -f $variant_file -o . 
+    python $script_folder/generate_input.py -f $variant_file -o $output_folder 
     echo "Input files Time taken: ${SECONDS} seconds" >> $variant_file.log
 
 
@@ -105,7 +113,7 @@ elif [ "$mode" == "ganno" ]; then
     awk '{print $5"\t"$6}' $input_folder/genomic_position_GR${refgenome}_gene_var.tsv | sort | uniq > $input_folder/gene_var_tab_from_transvar.tsv
 
     echo "Generating input files: genevar, idvar, geneseq, ESM, VariPred"
-    python $script_folder/generate_input.py -f $input_folder/gene_var_tab_from_transvar.tsv -o .
+    python $script_folder/generate_input.py -f $input_folder/gene_var_tab_from_transvar.tsv -o $output_folder
     echo "Input files Time taken: ${SECONDS} seconds" >> $variant_file.log
 
 else
@@ -114,12 +122,10 @@ else
 fi
 # Precomputed Predictions #
 
-# # dbNSFP
+# # # dbNSFP
 if [ "$mode" == "ganno" ]; then
     variant_file="$input_folder/gene_var_tab_from_transvar.tsv"
 fi
-dbnsfp_folder="$predictions_folder/dbNSFP"
-dbnsfp_output="$dbnsfp_folder/dbNSFP_output.tsv"
 
 SECONDS=0
 
@@ -136,7 +142,9 @@ rm $dbnsfp_output.chr*
 python  $script_folder/dbNSFP/clean_dbnsfp_data.py -f $variant_file \
                                                    -int $script_folder/intermediate_files \
                                                    -df $dbnsfp_output \
+                                                   -inp $input_folder \
                                                    -o $dbnsfp_output.cleaned
+                                                   
 
 
 awk '$2 != "." {print $1":"$2"-"$2"\t"$3"\t"$4}' $input_folder/dbnsfp_genomic_position_GR37.tsv  > $input_folder/dbnsfp_genomic_position_GR37_tabix.tsv
@@ -147,7 +155,7 @@ echo "dbNSFP Time taken: ${SECONDS} seconds" >> $variant_file.log
 SECONDS=0
 
 # # Variant database
-python $script_folder/extract_variant_databases.py -d .
+python $script_folder/extract_variant_databases.py -d $output_folder --databases $database_folder 
 echo "VEP Time taken: ${SECONDS} seconds" >> $variant_file.log
 
 # # Web servers #
@@ -160,24 +168,32 @@ SECONDS=0
 #         -g 38  | sort | uniq | awk 'NR > 1 {print $1"\t"$2"\t"$3"\t"$4"\t"$7}' > $predictions_folder/PhDSNP/PhDSNP_predictions.tsv
 # echo "PHD Time taken: ${SECONDS} seconds" >> $variant_file.log
 
+
+# docker run -v /home/your_home:/home/your_home  phd-snpg:full] \
+#                 /home/bass/PhD-SNPg/predict_variants.py /home/bass/PhD-SNPg/test/test_short_variants_hg19.tsv -g hg19
+
+                
 # # # # SuSPect
 SECONDS=0
 
-echo SuSPect
+# echo SuSPect
+#ls -lahrt /BenchVEP/scripts/
+#ls -lahrt /BenchVEP
 mkdir -p $predictions_folder/SuSPect/
-perl $suspect_folder/suspect.pl --input $input_folder/id_var_tab.txt --output $predictions_folder/SuSPect/SuSPect_predictions_tmp.tsv
-awk  -F "\t" '{print $1"\t"$2"\t"$6}' $predictions_folder/SuSPect/SuSPect_predictions_tmp.tsv > $predictions_folder/SuSPect/SuSPect_predictions.tsv
+perl $script_folder/suspect/suspect_package/suspect.pl --input $input_folder/id_var_tab.txt --output $predictions_folder/SuSPect/SuSPect_predictions_tmp.tsvaa
+
+awk  -F "\t" '{print $1"\t"$2"\t"$6}' $predictions_folder/SuSPect/SuSPect_predictions_tmp.tsvaa > $predictions_folder/SuSPect/SuSPect_predictions.tsv
 rm $predictions_folder/SuSPect/SuSPect_predictions_tmp.tsv
 echo "SuSPECT Time taken: ${SECONDS} seconds" >> $variant_file.log
 
-# GPU predictions #
+# # GPU predictions #
 
-# ESM
-#bash $predictions_folder/ESM/run_esm.sh
-#bash $script_folder/cleaner/clean_ESM.sh . 
+# # ESM
+# #bash $predictions_folder/ESM/run_esm.sh
+# #bash $script_folder/cleaner/clean_ESM.sh . 
 
-# Concatenate all predictions
+# # Concatenate all predictions
 
-python $script_folder/concat_predictions.py -d . -f $variant_file -m $mode -n $output_name
+# python $script_folder/concat_predictions.py -d . -f $variant_file -m $mode -n $output_name
 
-echo "Done"
+# echo "Done"
