9eaf5f25ab8b6e527b4a1ef617b92a6f7cf8c26f
ragouradja <ragou.radja@gmail.com>
2024-07-21 20:10:04 +0200

    keeping only wget commands

--- scripts/download_databases.sh
+++ scripts/download_databases.sh
@@ -35,17 +35,13 @@ wget "http://prodata.swmed.edu/DBSAV/download/humanSAV.txt.gz" -P $database_fold
 # Envision
 mkdir -p $database_folder/Envision
 wget "https://envision.gs.washington.edu/shiny/downloads/human_predicted_combined_20170925.csv.bz2" -P $database_folder/Envision
-# Check if need redirect
-bzip2 -d $database_folder/Envision/human_predicted_combined_20170925.csv.bz2
-awk -F "," 'NR>2 {print $6"\t"$8"\t"$NF}' $database_folder/Envision/human_predicted_combined_20170925.csv  > $database_folder/Envision/Envision_clean.tsv
+
 
 # InMeRF (to index)
 mkdir -p $database_folder/InMeRF
 wget "https://www.med.nagoya-u.ac.jp/neurogenetics/InMeRF/download/InMeRF_score_hg38.txt.gz" -P $database_folder/InMeRF
 zcat $database_folder/InMeRF/InMeRF_score_hg38.txt.gz | sort -k1,1 -k2,2n > $database_folder/InMeRF/InMeRF_score_hg38.txt
-rm $database_folder/InMeRF/InMeRF_score_hg38.txt.gz
-bgzip $database_folder/InMeRF/InMeRF_score_hg38.txt
-tabix -p vcf -s 1 -b 2 -e 2 $database_folder/InMeRF/InMeRF_score_hg38.txt.gz
+
 
 # LASSIE (with index)
 mkdir -p $database_folder/LASSIE
@@ -56,25 +52,20 @@ wget "http://compgen.cshl.edu/LASSIE/data/LASSIE_fitness_effect_hg19.tsv.gz.tbi"
 mkdir -p $database_folder/MISTIC
 wget "https://lbgi.fr/mistic/static/data/MISTIC_GRCh38.tsv.gz" -P $database_folder/MISTIC
 zcat $database_folder/MISTIC/MISTIC_GRCh38.tsv.gz | sed "s/chr//g" | sort -k1,1 -k2,2n > $database_folder/MISTIC/MISTIC_GRCh38.tsv
-rm $database_folder/MISTIC/MISTIC_GRCh38.tsv.gz
-bgzip $database_folder/MISTIC/MISTIC_GRCh38.tsv
-tabix -p vcf -s 1 -b 2 -e 2 $database_folder/MISTIC/MISTIC_GRCh38.tsv.gz
+
 
 # MutFormer (to index)
 mkdir -p $database_folder/MutFormer
 wget "http://www.openbioinformatics.org/mutformer/hg19_MutFormer.zip" -P $database_folder/MutFormer
 unzip $database_folder/MutFormer/hg19_MutFormer.zip -d $database_folder/MutFormer
 sort -k1,1 -k2,2n $database_folder/MutFormer/hg19_MutFormer.txt > $database_folder/MutFormer/hg19_MutFormer_sorted.tsv
-bgzip $database_folder/MutFormer/hg19_MutFormer_sorted.tsv
-tabix -p vcf -s 1 -b 2 -e 2 $database_folder/MutFormer/hg19_MutFormer_sorted.tsv.gz
+
 
 # MutScore (to index)
 mkdir -p $database_folder/MutScore
 wget "https://storage.googleapis.com/rivolta_mutscore/mutscore-v1.0-hg38.tsv.gz" -P $database_folder/MutScore
 zcat $database_folder/MutScore/mutscore-v1.0-hg38.tsv.gz | sort -k1,1 -k2,2n > $database_folder/MutScore/mutscore-v1.0-hg38_sorted.tsv
-rm $database_folder/MutScore/mutscore-v1.0-hg38_sorted.tsv.gz
-bgzip $database_folder/MutScore/mutscore-v1.0-hg38_sorted.tsv
-tabix -p vcf -s 1 -b 2 -e 2 $database_folder/MutScore/mutscore-v1.0-hg38_sorted.tsv.gz
+
 
 # PONP2 (site is down)
 # Create directory
@@ -85,9 +76,7 @@ mkdir -p $database_folder/PONP2
 mkdir -p $database_folder/SIGMA
 wget "https://sigma-pred.org/api/sigma/download?name=sigma_scores" -O $database_folder/SIGMA/sigma_scores.txt
 zcat $database_folder/SIGMA/sigma_scores.txt | sort -k1,1 -k2,2n > $database_folder/SIGMA/sigma_scores_sorted.txt
-rm $database_folder/SIGMA/sigma_scores_sorted.txt.gz
-bgzip $database_folder/SIGMA/sigma_scores_sorted.txt
-tabix -p vcf -s 1 -b 2 -e 2 $database_folder/SIGMA/sigma_scores_sorted.txt.gz
+
 
 # UNEECON with index
 mkdir -p $database_folder/UNEECON
@@ -106,21 +95,6 @@ mkdir $database_folder/dbNSFP/tabix_19
 
 wget "https://usf.box.com/shared/static/nqgw17r4zzuluk5ginqm33hhopiak26c" -O $database_folder/dbNSFP/dbNSFP4.7a.zip
 unzip $database_folder/dbNSFP/dbNSFP4.7a.zip -d $database_folder/dbNSFP/
-# rm $database_folder/dbNSFP/dbNSFP4.7a.zip,:;  
-for chrom in {1..22} X Y; 
-do 
-    echo "Indexing chromosome $chrom...";
-    zcat $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz | { head -n 1; tail -n +2 | sort -T tmp -k1,1 -k2,2n -k9,9n | awk '$9 != "."'; } > $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}
-    bgzip -f $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}
-    tabix -p vcf -s 1 -b 2 -e 2 $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz
-    mv $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz.tbi $database_folder/dbNSFP/tabix_38
-    tabix -p vcf -s 1 -b 9 -e 9 $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz
-    mv $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz.tbi $database_folder/dbNSFP/tabix_19
-
-done 
-
-# Extract headers from dbNSFP files
-python $script_folder/dbNSFP/dbnsfp_header.py $database_folder/dbNSFP/dbNSFP4.7a_variant.chr10.gz $script_folder
 
 # Download SuSPect files
 mkdir -p ./scripts/suspect/
@@ -128,24 +102,15 @@ wget http://www.sbg.bio.ic.ac.uk/~suspect/suspect_package-v1.3.tar.gz -P ./scrip
 tar -xvf ./scripts/suspect/suspect_package-v1.3.tar.gz -C ./scripts/suspect/
 sed "s,/bmm/www/servers/suspect,./scripts/suspect/suspect_package," ./scripts/suspect/suspect_package/suspect.pl -i
 sed "s,/suspect_package/data/,data/," ./scripts/suspect/suspect_package/suspect.pl -i
-perl -MCPAN -e 'install DBI'
-perl -MCPAN -e 'install DBD::SQLite'
+
  
 # Download PhD files
 
 
 # # Reference genome hg38
 wget "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz" -P $transvar_folder/hg38
-gunzip $transvar_folder/hg38/hg38.fa.gz
-samtools faidx $transvar_folder/hg38/hg38.fa.gz
-transvar config -k reference -v $(realpath transvar/hg38/hg38.fa) --refversion hg38
-transvar config -k ensembl -v $(realpath transvar/hg38/hg38.ensembl.gtf.gz.transvardb) --refversion hg38
 
 # # Reference genome hg19
 wget "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz" -P $transvar_folder/hg19
-gunzip $transvar_folder/hg19/hg19.fa.gz
-samtools faidx $transvar_folder/hg19/hg19.fa.gz
-transvar config -k reference -v $(realpath transvar/hg19/hg19.fa) --refversion hg19
-transvar config -k ensembl -v $(realpath transvar/hg19/hg19.ensembl.gtf.gz.transvardb) --refversion hg19
 
 
