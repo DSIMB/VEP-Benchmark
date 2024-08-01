0f928800c9f56b25de470f8bb5068ef7867a28cc
ragouradja <ragou.radja@gmail.com>
2024-08-01 20:01:34 +0200

    zenodo maj

--- scripts/configure_databases.sh
+++ scripts/configure_databases.sh
@@ -9,88 +9,40 @@ script_folder="$path_data/scripts"
 
 
 # # Reference genome hg38
-# echo "Configuring reference genome hg38..."
-# gunzip $transvar_folder/hg38/hg38.fa.gz
-# samtools faidx $transvar_folder/hg38/hg38.fa
+echo "Configuring reference genome hg38..."
+gunzip $transvar_folder/hg38/hg38.fa.gz
+samtools faidx $transvar_folder/hg38/hg38.fa
 transvar config -k reference -v $(realpath $path_data/transvar/hg38/hg38.fa) --refversion hg38
 transvar config -k ensembl -v $(realpath $path_data/transvar/hg38/hg38.ensembl.gtf.gz.transvardb) --refversion hg38
 
 # # Reference genome hg19
-# echo "Configuring reference genome hg19..."
-# gunzip $transvar_folder/hg19/hg19.fa.gz
-# samtools faidx $transvar_folder/hg19/hg19.fa
+echo "Configuring reference genome hg19..."
+gunzip $transvar_folder/hg19/hg19.fa.gz
+samtools faidx $transvar_folder/hg19/hg19.fa
 transvar config -k reference -v $(realpath $path_data/transvar/hg19/hg19.fa) --refversion hg19
 transvar config -k ensembl -v $(realpath $path_data/transvar/hg19/hg19.ensembl.gtf.gz.transvardb) --refversion hg19
 
 
 
-# # Envision
-# echo "Configuring Envision database..."
-# bzip2 -d $database_folder/Envision/human_predicted_combined_20170925.csv.bz2
-# awk -F "," 'NR>2 && $6 != "" {print $6"\t"$8"\t"$NF}' $database_folder/Envision/human_predicted_combined_20170925.csv | sort -k1,1 -k2,2 > $database_folder/Envision/Envision_clean.tsv
-# python $script_folder/create_index.py --file $database_folder/Envision/Envision_clean.tsv -i 0
-
-# # DeepSAV
-# echo "Configuring DeepSAV database..."
-# zcat $database_folder/DeepSAV/humanSAV.txt.gz | awk '{print $1"\t"$3$2$4"\t"$5}' | uniq  | sort -k1,1 -k2,2 | gzip > $database_folder/DeepSAV/humanSAV_light.txt.gz
-# python $script_folder/create_index.py --file $database_folder/DeepSAV/humanSAV_light.txt.gz -i 0
-
-
 
 # # InMeRF (to index)
-# echo "Configuring InMeRF database..."
-# bgzip -f $database_folder/InMeRF/InMeRF_score_hg38.txt
-# tabix -p vcf -s 1 -b 2 -e 2 $database_folder/InMeRF/InMeRF_score_hg38.txt.gz
+echo "Configuring InMeRF database..."
+bgzip -f $database_folder/InMeRF/InMeRF_score_hg38.txt
+tabix -p vcf -s 1 -b 2 -e 2 $database_folder/InMeRF/InMeRF_score_hg38.txt.gz
 
-# # MISTIC (to index)
-# echo "Configuring MISTIC database..."
-# bgzip -f $database_folder/MISTIC/MISTIC_GRCh38.tsv
-# tabix -p vcf -s 1 -b 2 -e 2 $database_folder/MISTIC/MISTIC_GRCh38.tsv.gz
-
-# # MutFormer (to index)
-# echo "Configuring MutFormer database..."
-# bgzip $database_folder/MutFormer/hg19_MutFormer_sorted.tsv
-# tabix -p vcf -s 1 -b 2 -e 2 $database_folder/MutFormer/hg19_MutFormer_sorted.tsv.gz
-
-# # MutScore (to index)
-# echo "Configuring MutScore database..."
-# bgzip -f $database_folder/MutScore/mutscore-v1.0-hg38_sorted.tsv
-# tabix -p vcf -s 1 -b 2 -e 2 $database_folder/MutScore/mutscore-v1.0-hg38_sorted.tsv.gz
-
-# # SIGMA (to index)
-# echo "Configuring SIGMA database..."
-# bgzip -f $database_folder/SIGMA/sigma_scores_sorted.txt
-# tabix -p vcf -s 1 -b 2 -e 2 $database_folder/SIGMA/sigma_scores_sorted.txt.gz
-
-# # dbNSFP4.7a
-# echo "Configuring dbNSFP4.7a database..."
-# for chrom in {1..22} X Y; 
-# do 
-#     echo "Indexing chromosome $chrom..."
-#     zcat $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz | { head -n 1; tail -n +2 | sort -k1,1 -k2,2n -k9,9n | awk '$9 != "."'; } > $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}
-#     bgzip -f $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}
-#     tabix -p vcf -s 1 -b 2 -e 2 $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz
-#     mv $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz.tbi $database_folder/dbNSFP/tabix_38
-#     tabix -p vcf -s 1 -b 9 -e 9 $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz
-#     mv $database_folder/dbNSFP/dbNSFP4.7a_variant.chr${chrom}.gz.tbi $database_folder/dbNSFP/tabix_19
-# done 
-
-# # Extract headers from dbNSFP files
-# echo "Extracting headers from dbNSFP files..."
-# python $script_folder/dbNSFP/dbnsfp_header.py $database_folder/dbNSFP/dbNSFP4.7a_variant.chr10.gz $script_folder
 
 # Download SuSPect files
-# echo "Setting up SuSPect database..."
-# tar -xvf $script_folder/suspect/suspect_package-v1.3.tar.gz -C $script_folder/suspect/
-# sed "s,/bmm/www/servers/suspect,$script_folder/suspect/suspect_package," $script_folder/suspect/suspect_package/suspect.pl -i
-# sed "s,/suspect_package/data/,data/," $script_folder/suspect/suspect_package/suspect.pl -i
+echo "Setting up SuSPect database..."
+tar -xvf $script_folder/suspect/suspect_package-v1.3.tar.gz -C $script_folder/suspect/
+sed "s,/bmm/www/servers/suspect,$script_folder/suspect/suspect_package," $script_folder/suspect/suspect_package/suspect.pl -i
+sed "s,/suspect_package/data/,data/," $script_folder/suspect/suspect_package/suspect.pl -i
 
 
-# echo "Setting up PhD-SNPg database..."
-# mkdir -p $script_folder/phdsnp/
-# git clone https://github.com/biofold/PhD-SNPg $script_folder/phdsnp
-# echo "Running $script_folder/phdsnp/setup.py from PhD-SNPg github... \
-# (can take hours, output text will be in $script_folder/phdsnp/.setup_output if needed)"
-# eval "$(conda shell.bash hook)"
-# conda activate phdsnp
-# python2 $script_folder/phdsnp/setup.py install linux.x86_64 >  $script_folder/phdsnp/.setup_output
+echo "Setting up PhD-SNPg database..."
+mkdir -p $script_folder/phdsnp/
+git clone https://github.com/biofold/PhD-SNPg $script_folder/phdsnp
+echo "Running $script_folder/phdsnp/setup.py from PhD-SNPg github... \
+(can take hours, output text will be in $script_folder/phdsnp/.setup_output if needed)"
+eval "$(conda shell.bash hook)"
+conda activate phdsnp
+python2 $script_folder/phdsnp/setup.py install linux.x86_64 >  $script_folder/phdsnp/.setup_output
