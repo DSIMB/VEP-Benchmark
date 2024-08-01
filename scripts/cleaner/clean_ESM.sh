97b1b79d077a7c60207ef12e15beba5e1f7801ac
ragouradja <ragou.radja@gmail.com>
2024-08-01 18:14:56 +0200

    clean script

--- scripts/cleaner/clean_ESM.sh
+++ scripts/cleaner/clean_ESM.sh
@@ -1,4 +1,3 @@
-folder=$1
-
-esm_folder=$folder/tools/ESM/
+predictions_folder=$1
+esm_folder=$predictions_folder/ESM/
 for file in $esm_folder/*csv; do f="$(basename -- $file)"; protein=${f%.*}; awk -v prot=$protein -F ','  ' NR>1{print prot"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7}' $file ; done > $esm_folder/ESM_predictions.tsv
